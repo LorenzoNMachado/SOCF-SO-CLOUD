6a1775a6e4f93b296e51c07fa8434f34d9e181a7
LorenzoNMachado <168145675+LorenzoNMachado@users.noreply.github.com>
2025-11-11 22:49:50 +0000

    top

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import platform
+import psutil
+import os
+import json
+
+app = Flask(name)
+
+@app.route('/info')
+def info():
+    dados = {
+        "nome": "Lorenzo, João Abreu, Kevyn Gabriel"
+    }
+    return json.dumps(dados, ensure_ascii=False)
+@app.route('/metricas')
+def metricas():
+    metricas_dados = {
+        "pid": os.getpid(),
+        "sistema_operacional": platform.platform(),
+        "uso_cpu": psutil.cpu_percent(),
+        "uso_memoria_mb": psutil.virtual_memory().used // (1024 * 1024)
+    }
+    return json.dumps(metricas_dados, ensure_ascii=False)
+if name == 'main':
+    app.run(host='0.0.0.0', port=5000)
+
+APP = app
